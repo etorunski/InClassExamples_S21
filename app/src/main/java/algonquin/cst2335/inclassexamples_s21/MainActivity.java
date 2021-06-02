3ba2501d035d9bd825aeefd04c48941dd14c9d5d
etoru <abcde>
2021-06-02 13:17:30 -0400

    Finished examples for Week 4

--- app/src/main/java/algonquin/cst2335/inclassexamples_s21/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_s21/MainActivity.java
@@ -1,8 +1,15 @@
 package algonquin.cst2335.inclassexamples_s21;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button loginBtn = findViewById(R.id.nextPageButton);
+        Log.w("MainActivity", "In onCreate() - Loading Widgets" );
+
+        loginBtn.setOnClickListener( clk -> {
+            EditText et = findViewById(R.id.inputEditText);
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+
+            nextPage.putExtra("SomeInfo", et.getText().toString());
+            startActivity( nextPage );
+        });
     }
+
 }
---
+++ app/src/main/java/algonquin/cst2335/inclassexamples_s21/SecondActivity.java
@@ -0,0 +1,21 @@
+package algonquin.cst2335.inclassexamples_s21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent fromPrevious = getIntent();
+        String text = fromPrevious.getStringExtra("SomeInfo");
+
+
+
+    }
+}
