06710818265742dff0ce44641afaf50a8ab5b2cf
etoru <abcde>
2021-06-11 11:35:11 -0400

    Working example #3

--- app/src/main/java/algonquin/cst2335/inclassexamples_s21/ChatRoom.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_s21/ChatRoom.java
@@ -9,9 +9,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,7 @@ import java.util.ArrayList;
 public class ChatRoom extends AppCompatActivity {
 
     ArrayList<ChatMessage> messages = new ArrayList<>();//hold our typed messages
+    ChatAdapter adt  ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +33,21 @@ public class ChatRoom extends AppCompatActivity {
         EditText messageTyped = findViewById(R.id.messageEdit);
         Button send = findViewById(R.id.sendbutton);
         RecyclerView chatList = findViewById(R.id.myrecycler);
-        ChatAdapter adt ;
-        chatList.setAdapter( adt = new ChatAdapter()  );
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+
+
+        //add an adapter object to the RecyclerView
+       adt = new ChatAdapter() ;
+
+        //     build the list:
+        chatList.setAdapter(adt); //need onCreateView, onBindView, getCount
+        //   StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+      LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         chatList.setLayoutManager(layoutManager);
 
 
         send.setOnClickListener( click -> {
                     ChatMessage nextMessage = new ChatMessage( messageTyped.getText().toString()  );
-                    messages.add(nextMessage);
+                    messages.add(nextMessage);//adds to array list
                     //clear the edittext:
                     messageTyped.setText("");
                     //refresh the list:
@@ -53,6 +62,22 @@ public class ChatRoom extends AppCompatActivity {
 
         public MyRowViews(View itemView) { //itemView is a ConstraintLayout, that has <TextView> as sub-item
             super(itemView);
+
+            itemView.setOnClickListener(  clik ->
+            {
+                AlertDialog.Builder builder = new AlertDialog.Builder( ChatRoom.this );
+                builder.setMessage( "Do you want to delete this?" )
+                        .setTitle("Title").setPositiveButton("Yes", (dlg, clic) -> {
+
+                            int row = getAbsoluteAdapterPosition();
+                             messages.remove(row);//which message do you delete?
+                          //update list view:
+                            adt.notifyItemRemoved(row); })
+                        .setNegativeButton("No", (dlg, clic) -> {  })
+                        .create()
+                        .show();
+
+            });
 
             rowMessage = itemView.findViewById(R.id.message);
         }
@@ -72,21 +97,42 @@ public class ChatRoom extends AppCompatActivity {
         public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) { //position is which row we're building
 
             MyRowViews thisRowLayout = (MyRowViews)holder;
-            thisRowLayout.rowMessage.setText( "This is row"+ position );
+            thisRowLayout.rowMessage.setText( "This is row "+ position );//sets the text on the row
+            //set the date text:
         }
 
         @Override
         public int getItemCount() {
-            return messages.size(); //how many items to show? //row layout is match_parent
+            return  messages.size(); //how many items to show?
         }
     }
 
     private class ChatMessage {  //Data model for a message in a row
         public String message;
+        public int sendOrReceive;
+        public String timeSent;
 
         public ChatMessage (String s)
         {
             message = s;
+        }
+
+        public ChatMessage(String message, int sendOrReceive, String timeSent) {
+            this.message = message;
+            this.sendOrReceive = sendOrReceive;
+            this.timeSent = timeSent;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public int getSendOrReceive() {
+            return sendOrReceive;
+        }
+
+        public String getTimeSent() {
+            return timeSent;
         }
     }
 
