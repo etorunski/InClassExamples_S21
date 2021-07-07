bf58203e08cf2503c823e9f87a44d33935ad9361
Eric Torunski <etorunski@gmail.com>
2021-07-07 14:05:26 -0400

    created a fragment

--- app/src/main/java/algonquin/cst2335/inclassexamples_s21/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/inclassexamples_s21/MainActivity.java
@@ -1,6 +1,8 @@
 package algonquin.cst2335.inclassexamples_s21;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -9,6 +11,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //loads immediately
+
+
+        //need a fragment object:
+        ExampleFragment theFragment = new ExampleFragment(); //I created this in my other files.
+
+        FragmentManager fMgr = getSupportFragmentManager(); // only one object
+        FragmentTransaction tx = fMgr.beginTransaction();
+
+        //either add, remove, or replace:
+        tx.add(R.id.fragmentLocation, theFragment); //load theFragment into FrameLayout with id R.id.fragmentLocation
+
+        //finally commit:
+        tx.commit(); //start the loading...
+
+
+
     }
 }
